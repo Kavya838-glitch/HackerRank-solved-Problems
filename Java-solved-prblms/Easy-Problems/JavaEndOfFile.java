4c6b9870ee97f09dd4027aacccf2f7673fc8ced7
Kavya Y K <renukavenkatesh840@gmail.com>
2026-07-17 23:23:29 +0530

    Java HackerRank Problem solution Q.No.:9.

---
+++ Java-solved-prblms/Easy-Problems/JavaEndOfFile.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEndOfFile{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        
+            while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            System.out.println(n+" "+s);
+            n++;
+            
+            }        
+    }
+}       
